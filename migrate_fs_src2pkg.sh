427d32de88c20d0f640153bdcda0b1efcda5adf9
kismetgerald <kagbasi@digitainc.com>
2016-07-15 12:48:42 -0400

    Reorder Functions & Update Reademe
    reordered the placement of the various functions and updated the main
    function.  also made a minor adjustment to the title header in the
    readme

--- migrate_fs_src2pkg.sh
+++ migrate_fs_src2pkg.sh
@@ -5,32 +5,23 @@ fs_path="/usr/local/freeswitch"
 fpbx_path="/var/www/fusionpbx"
 
 # Functions
-unknown_os ()
+main ()
 {
-  echo "Unfortunately, your operating system distribution and version are not supported by this script."
-  echo
-  echo "You can override the OS detection by setting os= and dist= prior to running this script."
-  echo "You can find a list of supported OSes and distributions on our website: https://packagecloud.io/docs#os_distro_version"
-  echo
-  echo "For example, to force Ubuntu Trusty: os=ubuntu dist=trusty ./script.sh"
-  echo
-  echo "Please email support@packagecloud.io and let us know if you run into any issues."
-}
+  detect_os
+  switch_check
 
-switch_check ()
-{
-echo "Checking for the existence of FreeSWITCH..."
-  if [ -d "$fs_path" ]
-    then
-      echo "Excellent!  The FreeSWITCH directory ($fs_path) was found so let's continue."
-      echo ""
-      detect_os
-    else
-      echo "Error: Directory $fs_path does not exists."
-      echo ""
-      echo "The traditional FreeSWITCH path ($fs_path) was not detected, as such, this script will stop execution."
-      echo ""
+  echo "$LICENSE"
+
+  read -p "Shall we proceed? [Y/N] " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]
+  then
+    echo "Operating System detected is =====>>>> $os/$dist"
+    echo "Existing FreeSWITCH detected at =====>>>> $fs_path"
+    echo "FusionPBX detected at =====>>>> $fpbx_path"
+  
   fi
+
 }
 
 detect_os ()
@@ -81,69 +72,74 @@ detect_os ()
   dist="${dist// /}"
 
   echo "Detected operating system as $os/$dist."
+  echo ""
 }
 
-main ()
+unknown_os ()
 {
-  detect_os
-  switch_check
+  echo "Unfortunately, your operating system distribution and version are not supported by this script."
+  echo "Please consider using the manual approach, if you know what you're doing!"
+  echo ""
+  echo "Exiting...."
+  echo ""
+}
 
-  # Need to first run apt-get update so that apt-transport-https can be
-  # installed
-  echo "Operating System detected is =====>>>> $os/$dist"
-  echo "Existing FreeSWITCH detected at =====>>>> $fs_path"
-  echo
+switch_check ()
+{
+echo "Checking for the existence of FreeSWITCH..."
+  if [ -d "$fs_path" ]
+    then
+      echo "Excellent!  The FreeSWITCH directory ($fs_path) was found so let's continue."
+      echo ""
+      detect_os
+    else
+      echo "Error: Directory $fs_path does not exists."
+      echo ""
+      echo "The traditional FreeSWITCH path ($fs_path) was not detected, as such, this script will stop execution."
+      echo ""
+  fi
 }
 
 LICENSE=$( cat << DELIM
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                                                                                                                       #
-# "THE UAYOR LICENSE" (Version 1)                                                                                       #
-#                                                                                                                       #
-# This is the Use At Your Own Risk (UAYOR) License.                                                                     #
-# I, Kismet Agbasi, wrote this script.  As long as you retain this notice you can do whatever you want with it. This    #
-# script is just my basic attempt to help automate the process of switching FreeSWITCH from source to packages on an    #
-# existing and fully operational FusionBPX server.  I am by no means an expert and this script is not intended to be    #
-# super advanced in anyway.                                                                                             #
-#                                                                                                                       #
-# If you appreciate the rudimentary work and feel you can contribute to making it better in anyway, please consider     #
-# contributing some code via my Github repo.                                                                            #
-#                                                                                                                       #
-# Author:                                                                                                               #
-#   Kismet Agbasi <kagbasi@digitainc.com>                                                                               #
-#                                                                                                                       #
-# Contributor(s):                                                                                                       #
-#   <could you some - email me if you're interested>                                                                    #
-#                                                                                                                       #
+#
+# "THE UAYOR LICENSE" (Version 1)
+#
+# This is the Use At Your Own Risk (UAYOR) License.
+# I, Kismet Agbasi, wrote this script.  As long as you retain this notice you can do whatever you want with it. This
+# script is just my basic attempt to help automate the process of switching FreeSWITCH from source to packages on an
+# existing and fully operational FusionBPX server.  I am by no means an expert and this script is not intended to be
+# super advanced in anyway.
+#
+# If you appreciate the rudimentary work and feel you can contribute to making it better in anyway, please consider
+# contributing some code via my Github repo.
+#
+# Author:
+#   Kismet Agbasi <kagbasi@digitainc.com>
+#
+# Contributor(s):
+#   <could you some - email me if you're interested>
+#
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-#                                                                                                                       #
-# SOME THINGS TO NOTE BEFORE WE PROCEED:                                                                                #
-#                                                                                                                       #
-# This script will automatically check your system to ensure it is supported and that a source installation             #
-# of FreeSWITCH actually exists.  The traditional path ($fs_path) is used for this check.                               #
-# Additionally, there are some files we will need from the FusionPBX installation folders, so if you don't have         #
-# them this script will fetch them for you and place them in ($fpbx_path).                                              #
-#                                                                                                                       #
-# NOTE:  If the FreeSWITCH path is not detected this script will not continue!!!                                        #
-#                                                                                                                       #
-# VERY IMPORTANT:  IF YOU HAVE NOT BACKED UP YOUR SYSTEM, DO SO NOW!!!                                                  #
-#                                                                                                                       #
-# I take no responsibility for a failed system as a result of using this script.  If anything should go wrong           #
-# a proper backup should be easy to restore.  If you proceed without taking a backup, YOU ARE FULLY RESPONSIBLE!!!      #
-#                                                                                                                       #
+#
+# SOME THINGS TO NOTE BEFORE WE PROCEED:
+#
+# This script will automatically check your system to ensure it is supported and that a source installation
+# of FreeSWITCH actually exists.  The traditional path ($fs_path) is used for this check.
+# Additionally, there are some files we will need from the FusionPBX installation folders, so if you don't have
+# them this script will fetch them for you and place them in ($fpbx_path).
+#
+# NOTE:  If the FreeSWITCH path is not detected this script will not continue!!!
+#
+# VERY IMPORTANT:  IF YOU HAVE NOT BACKED UP YOUR SYSTEM, DO SO NOW!!!
+#
+# I take no responsibility for a failed system as a result of using this script.  If anything should go wrong
+# a proper backup should be easy to restore.  If you proceed without taking a backup, YOU ARE FULLY RESPONSIBLE!!!
+#
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 DELIM
 )
 
-echo "$LICENSE"
-
-read -p "Shall we proceed? [Y/N] " -n 1 -r
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-  echo "Operating System detected is =====>>>> $os/$dist"
-  echo "Existing FreeSWITCH detected at =====>>>> $fs_path"
-  echo "FusionPBX detected at =====>>>> $fpbx_path"
-  
-fi
+# BEGIN SCRIPT EXECUTION
+main
 exit 1
